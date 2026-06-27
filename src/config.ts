0db4e72dd99e61889a94af90bf1d98ffaf97c337
marukun712 <marukun530@gmail.com>
2026-06-27 15:55:32 +0900

    タイムライン追加

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 export const SITE_TITLE = "maril.blue";
 export const SITE_URL = "https://maril.blue";
 export const GITHUB_URL = "https://github.com/marukun712";
+
+export const TIMELINE_NAME = "maril";
+export const TIMELINE_ICON = "https://pbs.twimg.com/media/G4UfqRNaMAATVYa?format=jpg&name=small";
+export const TIMELINE_BIO = "hogefuga";
